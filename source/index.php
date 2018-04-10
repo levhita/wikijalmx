b712e603839d77ef6eff11175b7a322446884e6e
Levhita <levhita@gmail.com>
2018-04-09 23:39:52 -0500

    basic template compiling system

---
+++ source/index.php
@@ -0,0 +1,8 @@
+<?php
+$_TITLE = "Index";
+include("../views/header.php");
+?>
+
+<p>This is some content for index</p>
+
+<?php include("../views/footer.php"); ?>
---
+++ views/header.php
@@ -0,0 +1,6 @@
+<html>
+	<head>
+		<title><?=$_TITLE?></title>
+	</head>
+	<body>
+		<h1><?=$_TITLE?></h1>
